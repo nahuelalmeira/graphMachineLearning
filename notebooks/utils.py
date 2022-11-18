cab76280cfb9051c1ce2e9f8c278dbc3045b213a
nahuelalmeira <nahu1990@gmail.com>
2022-11-18 15:03:38 -0300

    replace nx functions by numpy implementations

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,6 +1,8 @@
+import igraph as ig
 import networkx as nx
 
-def get_CI_nx(g: nx.Graph):
+
+def get_CI_nx(g: nx.Graph) -> list:
     """
     Compute Collective Influence of each node at length 1.
     """
@@ -8,5 +10,17 @@ def get_CI_nx(g: nx.Graph):
     for i, v in enumerate(g.nodes()):
         nn_influence = sum([g.degree(w) - 1 for w in g.neighbors(v)])
         ci = (g.degree(v) - 1) * nn_influence
+        ci_values.append(ci)
+    return ci_values
+
+def get_CI_ig(g: ig.Graph()) -> list:
+    """
+    Compute Collective Influence of each node at length 1.
+    """
+    ci_values = []
+    for v, k in enumerate(g.degree()):
+        nn_influence = sum([g.degree()[w] - 1 for w in g.neighbors(v)])
+        #print(g.neighborhood(v))
+        ci = (k - 1) * nn_influence
         ci_values.append(ci)
     return ci_values
